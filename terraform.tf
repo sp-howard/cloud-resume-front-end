cf1bb4b85c1c406b2502af9a7cd3511446b8a37f
sp-howard <stevenphoward@gmail.com>
2023-07-08 14:53:41 -0700

    Chnaged folder structure and added GH Actions

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,8 @@ terraform {
     organization = "sp-howard"
 
     workspaces {
-      name = "cloud-resume-front-end"
+      # name = "cloud-resume-front-end"
+      name = "cloud-resume-front-end-gh"
     }
   }
 
